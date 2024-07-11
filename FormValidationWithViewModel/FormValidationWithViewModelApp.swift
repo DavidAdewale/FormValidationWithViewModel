4fd514291270aa3932a2a4940545411337a84701
DavidAdewale <adewaledavidola@gmail.com>
2024-07-11 17:42:13 +0100

    Initial Commit

---
+++ FormValidationWithViewModel/FormValidationWithViewModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationWithViewModelApp.swift
+//  FormValidationWithViewModel
+//
+//  Created by David Adewale on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationWithViewModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
